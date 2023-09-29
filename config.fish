37943f10480abc7c78bd516b550840d6508acade
n-pall <npall@thesummitgrp.com>
2023-09-29 15:31:37 +0530

    add push pull aliases

--- config.fish
+++ config.fish
@@ -12,7 +12,17 @@ function ll
 alias g=git
 alias gco="git checkout"
 alias gac="git ac"
+alias gpl="git pull origin"
+alias gps="git push origin"
 
 # set FISHCONFIG ~/.config/fishl
 
 set fish_key_bindings fish_user_key_bindings
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
