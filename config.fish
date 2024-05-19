8576503357b994a6d790a70c02712bdee0f8e4b0
PRHYME <nishantpall877@gmail.com>
2024-05-19 16:28:00 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -12,8 +12,12 @@ function ll
 alias g=git
 alias gco="git checkout"
 alias gac="git ac"
+alias glo="git log --oneline"
 alias gpl="git pull origin"
 alias gps="git push origin"
+alias gr="git rebase"
+alias grba="git rebase --abort"
+alias grbc="git rebase --continue"
 alias pip=pip3
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 
